70202b8f9f937c398bfddfa779b6b1292afafceb
Jim Geovedi <jim.geovedi@gmail.com>
2013-04-16 00:40:33 +0700

    zipf + empirical sense & sensibility

--- zipf.py
+++ zipf.py
@@ -3,7 +3,25 @@
 
 import codecs
 import matplotlib.pyplot as plt
+import functools
 from nltk import FreqDist
+
+maxfreq = 10**6
+
+
+class funcobj(object):
+    def __init__(self, mylen, f, *args, **kwargs):
+        self.f = functools.partial(f, *args, **kwargs)
+        self.mylen = mylen
+    def __getitem__(self, idx):
+        return self.f(idx + 1)
+    def __len__(self):
+        return self.mylen
+
+
+def zipf(k, rank):
+    return k / rank
+
 
 def main():
     corpora = ['idwiki', 'kaskus', 'kompas', 'twitter']
@@ -19,13 +37,16 @@ def main():
             ranks.append(rank + 1)
             freqs.append(fd[word])
 
-        plt.grid(True)
         plt.loglog(ranks, freqs, label=corpus)
-        plt.xlabel('rank', fontsize=14, fontweight='bold')
-        plt.ylabel('frequency', fontsize=14, fontweight='bold')
-        plt.legend(loc='upper right')
-        plt.savefig(corpus + '.png')
-        plt.close()
+
+    plt.loglog(ranks, funcobj(len(ranks), zipf, maxfreq), 
+               label='zipf\'s law')
+
+    plt.grid(True)
+    plt.xlabel('rank', fontsize=14, fontweight='bold')
+    plt.ylabel('frequency', fontsize=14, fontweight='bold')
+    plt.legend(loc='upper right')
+    plt.savefig('freqdist.png')
 
 if __name__ == '__main__':
     main()
